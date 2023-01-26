94387968be9f16dcd07acd64fe329492638113df
Mot3z <motazboss2001@gmail.com>
2023-01-26 11:40:40 +0200

    First Commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Motaz Abu Mandeel, a Flutter developer, Technical blog writer.';
+
+  static const String aboutMeDetail =
+      'I\'m a Fresh Graduate with ٍٍSoftware Development as Majors from Islamic University, Palestine. I have been developing mobile apps for over 2 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. I worked as a Flutter application developer at Cirrus  ';
+}
---
+++ lib/utils/contact_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContactUtils {
+  static const List<IconData> contactIcon = [
+    Icons.home,
+    Icons.phone,
+    Icons.mail,
+  ];
+
+  static const List<String> titles = [
+    "Location",
+    "Phone",
+    "Email",
+  ];
+
+  static const List<String> details = [
+    "Gaza, Palestine",
+    "(+970) 59-462-1570",
+    "motazabumandeel@gmail.com",
+  ];
+}
